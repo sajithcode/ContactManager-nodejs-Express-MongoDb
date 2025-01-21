54678305b738a42bd217f089160f63f44e5127f4
Sajith Rathna Sampath <sajithsampath721@gmail.com>
2025-01-21 22:18:58 +0530

    mongoose install and connect

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,14 +1,16 @@
+const asyncHandler = require('express-async-handler')
+
 //@desc Get All Contacts
 //@route GET /api/contacts
 //@access Public
-const getContacts = (req, res) => {
+const getContacts = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get all contacts" });
-};
+});
 
 //@desc Create New Contacts
 //@route POST /api/contacts
 //@access Public
-const createContact = (req, res) => {
+const createContact = asyncHandler(async (req, res) => {
   console.log("The request body is: ", req.body);
   const { name, email, phone } = req.body;
   if(!name || !email || !phone){
@@ -16,29 +18,29 @@ const createContact = (req, res) => {
     throw new Error("All fields are mandatory !");
   } 
   res.status(200).json({ message: "Create contacts" });
-};
+});
 
 //@desc Get  Contacts
 //@route GET /api/contacts
 //@access Public
-const getContact = (req, res) => {
+const getContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Get contacts for ${req.params.id}` });
-};
+});
 
 
 //@desc update Contact
 //@route PUT /api/contacts
 //@access Public
-const updateContact = (req, res) => {
+const updateContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update contacts for ${req.params.id}` });
-};
+});
 
 //@desc Delete Contact
 //@route DELETE /api/contacts
 //@access Public
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete contacts for ${req.params.id}` });
-};
+});
 
 module.exports = {
   getContacts,
